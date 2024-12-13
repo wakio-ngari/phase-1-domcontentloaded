29ac6118ea6dcef1cb94860ced7a33c4f527bce6
wakio-ngari <ngarijoyce017@gmail.com>
2024-12-13 10:49:00 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",
+    function(){
+        const paragraph=document.getElementById("text");
+
+        if (paragraph){
+            paragraph.textContent='This is really cool!';
+        }
+    });
